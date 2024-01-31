52689863744ded70f3333f5281ef55b4f0bf2add
Ke Wei || Luna <42366824+KWLuna@users.noreply.github.com>
2024-01-31 17:19:22 +0800

    Update ShapeCalculator.java

--- Exercises/src/ShapeCalculator.java
+++ Exercises/src/ShapeCalculator.java
@@ -1,4 +1,24 @@
 
 public class ShapeCalculator {
+	NumberCalculator numCalc = new NumberCalculator();
+	private double Pi = 3.14159265;
 
+	public double PerimeterOfCircle(double radius)
+	{
+		return numCalc.Multiply(numCalc.Multiply(2, radius), Pi);
+	}
+	public double AreaOfCircle(double radius)
+	{
+		return numCalc.Multiply(Pi, numCalc.Square(radius));
+	}
+	
+	public double PerimeterOfRectangle(double length, double width)
+	{
+		return numCalc.Multiply(numCalc.Add(length, width), 2);
+	}
+	public double AreaOfRectangle(double length, double width)
+	{
+		return numCalc.Multiply(length, width);
+	}
+	
 }
