0fde9f884916c2e2a30adf75d96eb64b0b861fc4
Ke Wei || Luna <42366824+KWLuna@users.noreply.github.com>
2024-01-31 17:18:15 +0800

    Update Calculator.java

--- Exercises/src/Calculator.java
+++ Exercises/src/Calculator.java
@@ -1,4 +1,12 @@
 
-public class Calculator {
-
+public interface Calculator {
+	public abstract double Add(double a, double b);
+	public abstract double Subtract(double a, double b);
+	public abstract double Multiply(double a, double b);
+	public abstract double Divide(double a, double b);
+	public abstract double Remainder(double a, double b);
+	public abstract double Square(double a);
+	
+	public abstract double Average(double[] a);
+	public abstract String Compare(double a, double b);
 }
