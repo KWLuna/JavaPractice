3b243395a0962ae274889159266e3482f6f584da
Ke Wei || Luna <42366824+KWLuna@users.noreply.github.com>
2024-01-31 17:18:28 +0800

    Update DateCalculator.java

--- Exercises/src/DateCalculator.java
+++ Exercises/src/DateCalculator.java
@@ -1,4 +1,23 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
 
 public class DateCalculator {
-
+	public String Method1()
+	{
+        long currentTimeInMillis = System.currentTimeMillis();
+        Date currentDate = new Date(currentTimeInMillis);
+        return currentDate.toString();
+	}
+	public String Method2()
+	{
+		// Create a SimpleDateFormat with a specific date and time format
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+	    // Set Timezone 
+	    sdf.setCalendar(Calendar.getInstance(TimeZone.getTimeZone("GMT")));
+	    // Display the current date and time in the specified format
+	    return sdf.format(System.currentTimeMillis());
+	}
 }
